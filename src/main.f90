fff543419959f74a9dae513edda6990d5ed9a678
Oooodette <odettebonnema@gmail.com>
2025-03-21 10:15:54 +0100

    GetTriples works with and new connectivity type

--- src/main.f90
+++ src/main.f90
@@ -1,13 +1,30 @@
 program  prog1
   use InputOutput
   use MolecularInformation
-
+  use DATA
   implicit none 
   character(len=100) :: FilePath
   type(Atom), pointer :: AtomList(:)
+  type(Bond), pointer :: TempBondlist(:), FinalBondList(:)
+  type(Triple), pointer :: TripleList(:)
+  type(Quadruple), pointer :: Quadlist(:)
+  type(StretchParameter) :: Stretch(2)
+  type(AngleParameter) :: Bending(3)
+  type(TorsionParameter) :: Torsional(3)
+  type(VanDerWaals) :: VDW(2)
+  type(ElectroStatic) :: Electrostatics(2)
+  type(Connectivity) :: Conn
+  integer :: nTriples
+  
   FilePath = 'c4h10.xyz'
 
   call ReadAtoms(FilePath, AtomList)
-
-  print *, Atomlist(1)%type
+  call InitiateParams(Stretch, Bending, Torsional, VDW, ElectroStatics)
+  call FindBonds(AtomList, Stretch, TempBondList, FinalBondList)
+  call BuildConnectivity(14, FinalBondList, Conn)
+  call GetTriples(conn, TripleList, nTriples)
+  ! call FindTriples(FinalBondList, TripleList)
+  ! call FindQuads(TripleList, QuadList)
+  print *, TripleList(1)
+  
 end program 
